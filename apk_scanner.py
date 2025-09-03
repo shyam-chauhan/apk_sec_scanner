3526c0508fc9a3322eb0092f5c00a6efad5ea70d
Chauhan Shyam <59696796+shyam-chauhan@users.noreply.github.com>
2025-09-03 12:15:52 +0530

    reduce base64 noise

--- apk_scanner.py
+++ apk_scanner.py
@@ -41,7 +41,10 @@ SENSITIVE_PATTERNS = {
     "Password": r"(?i)(?:password|passwd|pwd)[\s:=\"']+([a-zA-Z0-9!@#$%^&*()_+=\-]{4,})",
 }
 COMPILED_PATTERNS = {k: re.compile(v) for k, v in SENSITIVE_PATTERNS.items()}
-BASE64_REGEX = re.compile(r"\b[A-Za-z0-9+/]{20,}={0,2}\b")
+# uncomment this if you want base64 noise
+# BASE64_REGEX = re.compile(r"\b[A-Za-z0-9+/]{20,}={0,2}\b")
+# comment this afterwards to get base64, trust me you won't find anything
+BASE64_REGEX = re.compile(r"^(null)?$")  
 
 EXCLUDED_EXTENSIONS = ['.css', '.png', '.jpg', '.jpeg', '.gif', '.svg', '.ttf', '.otf', '.eot', '.woff', '.woff2']
 SENSITIVE_FILE_EXTENSIONS = ['.pem', '.key', '.crt', '.cer', '.pfx']
